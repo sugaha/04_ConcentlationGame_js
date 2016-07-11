e3a8a799ea3c75770f924cd76edd610634ad0ebe
sugaha <gorba1442@gmail.com>
2016-07-11 09:59:38 +0900

    神経衰弱

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-var gameArray = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7];
+var gameArray = [0,0,1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,10,10,11,11,12,12,13,13,14,14,15,15,16,16,17,17];
 var pickedTiles = [];
 var scoreText;
-var moves=0;
+var miss=0;
 var console_label;
 
 var shuffle = function(v){
@@ -28,15 +28,15 @@ var game = cc.Layer.extend({
         this._super();
         var gradient = cc.LayerGradient.create(cc.color(0,0,0,255), cc.color(0x46,0x82,0xB4,255));
         this.addChild(gradient);
-        scoreText = cc.LabelTTF.create("Moves: 0","Arial","32",cc.TEXT_ALIGNMENT_CENTER);
+        scoreText = cc.LabelTTF.create("Miss: 0","Arial","32",cc.TEXT_ALIGNMENT_CENTER);
         this.addChild(scoreText);
         scoreText.setPosition(90,50);
-        for(i=0;i<16;i++){
+        for(i=0;i<36;i++){
             var tile = new MemoryTile();
             tile.pictureValue = gameArray[i];
             this.addChild(tile,0);
             //タイルを格子状に配置する計算式
-            tile.setPosition(49+i%4*74,400-Math.floor(i/4)*74);
+            tile.setPosition(60+i%6*74,520-Math.floor(i/6)*70);
         }
     }
 });
@@ -59,23 +59,28 @@ var listener = cc.EventListener.create({
             var targetSize = target.getContentSize();
             var targetRectangle = cc.rect(0, 0, targetSize.width, targetSize.height);
             if (cc.rectContainsPoint(targetRectangle, location)) {
-                target.initWithFile("res/tile_"+target.pictureValue+".png");
+                target.initWithFile("res/c"+target.pictureValue+".jpg");
                 pickedTiles.push(target);
                 if(pickedTiles.length==2){
-                    checkTiles();
+                    checkMiss();
                 }
             }
         }
     }
 });
 
-function checkTiles(){
-    moves++;
-    scoreText.setString("Moves: "+moves);
+function checkMiss(){
+
+
     var pause = setTimeout(function(){
         if(pickedTiles[0].pictureValue!=pickedTiles[1].pictureValue){
             pickedTiles[0].initWithFile(res.cover_png);
             pickedTiles[1].initWithFile(res.cover_png);
+            miss++;
+            scoreText.setString("Miss: "+miss);
+            if(miss == 3){
+              cc.director.runScene(new thirdScean());
+            }
         }
         else{
             gameLayer.removeChild(pickedTiles[0]);
--- src/resource.js
+++ src/resource.js
@@ -1,14 +1,24 @@
 //resource.js
 var res = {
     cover_png : "res/cover.png",
-    tile_0_png : "res/tile_0.png",
-    tile_1_png : "res/tile_1.png",
-    tile_2_png : "res/tile_2.png",
-    tile_3_png : "res/tile_3.png",
-    tile_4_png : "res/tile_4.png",
-    tile_5_png : "res/tile_5.png",
-    tile_6_png : "res/tile_6.png",
-    tile_7_png : "res/tile_7.png",
+    c0_jpg : "res/c0.jpg",
+    c1_jpg : "res/c1.jpg",
+    c2_jpg : "res/c2.jpg",
+    c3_jpg : "res/c3.jpg",
+    c4_jpg : "res/c4.jpg",
+    c5_jpg : "res/c5.jpg",
+    c6_jpg : "res/c6.jpg",
+    c7_jpg : "res/c7.jpg",
+    c8_jpg : "res/c8.jpg",
+    c9_jpg : "res/c9.jpg",
+    c10_jpg : "res/c10.jpg",
+    c11_jpg : "res/c11.jpg",
+    c12_jpg : "res/c12.jpg",
+    c13_jpg : "res/c13.jpg",
+    c14_jpg : "res/c14.jpg",
+    c15_jpg : "res/c15.jpg",
+    c16_jpg : "res/c16.jpg",
+    c17_jpg : "res/c17.jpg"
 };
 
 var g_resources = [];
